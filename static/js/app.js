dca56c3825d010be52bde1a654e4a5527058d36d
Kaylon <kcyoung@tiksdd.com>
2020-11-13 19:25:30 -0600

    Updated code to populate dropdown list.

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,41 @@
 var url = 'samples.json'
 
-d3.json(url).then(function(data) {
-    console.log(data);
+
+// function optionChanged(id){
+//     console.log (id)
+// }
+
+
+
+function optionChanged(id) {
+    console.log(`this is an id: ${id}`)
+    d3.json(url).then(function(data) {
+        console.log(data);
+        
+        var names = data.names
+        var metadata = data.metadata
+        var samples = data.samples
+
+        console.log(names)
+        console.log(metadata)
+        console.log(samples)
+
+
+        d3.select("#selDataset")
+        .selectAll("option")
+        .data(names)
+        .enter() // creates placeholder for new data
+        .append("option") // appends a div to placeholder
+        .html(function(d) {
+            return `${d}`;
+        }); // sets the html in the div to an image tag with the link
+
+
+
+
+    });
+
+}
+
+optionChanged()
     
-
-    d3.select("#selDataset")
-    .selectAll("option")
-    .data(data.names)
-    .enter() // creates placeholder for new data
-    .append("option") // appends a div to placeholder
-    .html(function(d) {
-        return `${d}`;
-    }); // sets the html in the div to an image tag with the link
-  });
-
-  
